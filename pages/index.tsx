626affd2e03aeee26d352695cc946171f29fa71f
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-05 07:20:31 -0400

    Basic styles home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
 import { User, Units } from './types/user'
 import { Greenhouse, GreenhouseType } from './types/greenhouse'
 import { ControlMode, EnvironmentState, GreenhouseState, IpmState, LightningState } from './types/greenhouse-state'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardMedia, Chip, Icon, IconButton } from '@mui/material';
 
 function networkDelay() {
   const min = 100;
@@ -58,63 +62,149 @@ const Home: NextPage = () => {
     });
   }, networkDelay());
 
+  const styles = {
+    main: {
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr",
+      gridAutoRows: "1fr",
+      minHeight: "100vh",
+      maxWidth: "600px",
+      margin: "auto",
+      gap: 10,
+      padding: 10,
+    },
+    topSection: {
+      gridColumn: "1 / -1",
+      backgroundImage: "url(/images/home-top-background.webp)",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      color: "#fff",
+    },
+    stateSection: {
+      gridColumn: "1 / -1",
+      backgroundColor: "#d0cece",
+    },
+    pageLinkCard: {
+      height: "100%",
+    },
+    pageLinkAction: {
+      height: "100%",
+    },
+    pageLinkEnvironment: {
+      backgroundColor: "#f7caac"
+    },
+    pageLinkLighting: {
+      backgroundColor: "#fee599"
+    },
+    pageLinkIrrigation: {
+      backgroundColor: "#8da9db"
+    },
+    pageLinkPestControl: {
+      backgroundColor: "#a8d08c"
+    },
+    pageLinkContent: {
+      textAlign: "center",
+      display: "grid",
+      alignContent: "space-between",
+    },
+    cardChip: {
+      justifySelf: "end"
+    },
+  }
+
   return (
-    <div className={styles.container}>
+    <div style={styles.main}>
       <Head>
         <title>Langhe</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
-      <section className='top-section'>
-        <Link href="/settings">
-          <a>settings</a>
-        </Link>
-        <br />
-        <p>Hi, {user?.name}</p>
-      </section>
-      <section className='state-section'>
-
-      </section>
+      <Card sx={styles.topSection}>
+        <CardContent sx={{display: "flex", justifyContent: "space-between", alignItems: "end", height: "100%"}}>
+          <Typography variant='h4' color="inherit">
+            Hi, {user?.name}
+          </Typography>
+          <Link href="/settings">
+            <IconButton color="inherit">
+              <Icon>settings</Icon>
+            </IconButton>
+          </Link>
+        </CardContent>
+      </Card>
+      <Card sx={{ ...styles.stateSection }}>
+        <CardContent>
+        </CardContent>
+      </Card>
       <Link href="/environment">
         <a>
-          <section>
-            <h3>Environment</h3>
-            <p>{greenhouseState?.environment_state}</p>
-            <span>{greenhouseState?.environment_mode}</span>
-          </section>
+          <Card sx={{ ...styles.pageLinkCard, ...styles.pageLinkEnvironment }}>
+            <CardActionArea sx={styles.pageLinkAction}>
+              <CardContent sx={ styles.pageLinkContent }>
+                <Typography variant='h6' color="text.secondary">
+                  Environment
+                </Typography>
+                <Typography variant='h3' color="text.secondary">
+                  {greenhouseState?.environment_state}
+                </Typography>
+                <Chip label={greenhouseState?.environment_mode} sx={styles.cardChip} />
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </a>
       </Link>
-      <br />
       <Link href="/lighting">
         <a>
-          <section>
-            <h3>lighting</h3>
-            <p>{greenhouseState?.lighting_state}</p>
-            <span>{greenhouseState?.lighting_mode}</span>
-          </section>
+          <Card sx={{ ...styles.pageLinkCard, ...styles.pageLinkLighting }}>
+            <CardActionArea sx={styles.pageLinkAction}>
+              <CardContent sx={ styles.pageLinkContent }>
+                <Typography variant='h6' color="text.secondary">
+                  Lighting
+                </Typography>
+                <Typography variant='h3' color="text.secondary">
+                  {greenhouseState?.lighting_state}
+                </Typography>
+                <Chip label={greenhouseState?.lighting_mode} sx={styles.cardChip} />
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </a>
       </Link>
-      <br />
       <Link href="/irrigation">
         <a>
-          <section>
-            <h3>Environment</h3>
-            <p>??</p>
-            <span>??</span>
-          </section>
+          <Card sx={{ ...styles.pageLinkCard, ...styles.pageLinkIrrigation }}>
+            <CardActionArea sx={styles.pageLinkAction}>
+              <CardContent sx={ styles.pageLinkContent }>
+                <Typography variant='h6' color="text.secondary">
+                  Irrigation
+                </Typography>
+                <Typography variant='h3' color="text.secondary">
+                  ??
+                </Typography>
+                <Chip label="??" sx={styles.cardChip} />
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </a>
       </Link>
-      <br />
       <Link href="/pest-control">
         <a>
-          <section>
-            <h3>Pest Control</h3>
-            <p>??</p>
-            <span>??</span>
-          </section>
+          <Card sx={{ ...styles.pageLinkCard, ...styles.pageLinkPestControl }}>
+            <CardActionArea sx={styles.pageLinkAction}>
+              <CardContent sx={ styles.pageLinkContent }>
+                <Typography variant='h6' color="text.secondary">
+                  Pest Control
+                </Typography>
+                <Typography variant='h3' color="text.secondary">
+                  ??
+                </Typography>
+                <Chip label="??" sx={styles.cardChip} />
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </a>
       </Link>
-
     </div>
   )
 }
