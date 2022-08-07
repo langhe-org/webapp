6714a189cbfa92a103141d0aae3b12ae77a0edd6
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-07 01:56:07 -0400

    Basic components/styles of details pages

--- pages/irrigation.tsx
+++ pages/irrigation.tsx
@@ -1,18 +1,76 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import Icon from '@mui/material/Icon'
+import Head from 'next/head'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Switch from '@mui/material/Switch'
 
-const Irrigation: NextPage = () => {
+const styles = {
+  main: {
+    display: "grid",
+    padding: "10px",
+    gap: "10px",
+    justifyItems: "center",
+    height: "100vh",
+    alignItems: "start",
+  },
+  backButton: {
+    justifySelf: "start",
+  },
+  manualControlHeader: {
+    color: "#c0c0c0",
+    fontSize: "28px",
+  }
+};
+
+const Settings: NextPage = () => {
   return (
-    <div className={styles.container}>
-      Irrigation
-      <br />
+    <div style={styles.main}>
+      <Head>
+        <title>Langhe - Irrigation</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
 
       <Link href="/">
-        <a>back</a>
+        <a style={styles.backButton}>
+          <Button variant="text" startIcon={<Icon>arrow back</Icon>}>
+            Back
+          </Button>
+        </a>
       </Link>
+      <Typography variant='h1' sx={{ fontSize: 60 }}>
+        Irrigation
+      </Typography>
+
+      <Typography variant='h1' sx={styles.manualControlHeader}>
+        Manual Control
+      </Typography>
+      <FormControlLabel
+        value="auto"
+        control={<Switch />}
+        label="Zone 1"
+      />
+      <FormControlLabel
+        value="auto"
+        control={<Switch />}
+        label="Zone 2"
+      />
+      <FormControlLabel
+        value="auto"
+        control={<Switch />}
+        label="Zone 3"
+      />
+      <FormControlLabel
+        value="auto"
+        control={<Switch />}
+        label="Zone 4"
+      />
     </div>
   )
 }
 
-export default Irrigation
+export default Settings
--- pages/pest-control.tsx
+++ pages/pest-control.tsx
@@ -1,18 +1,75 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import Icon from '@mui/material/Icon'
+import Head from 'next/head'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Switch from '@mui/material/Switch'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 
-const PestControl: NextPage = () => {
+const styles = {
+  main: {
+    display: "grid",
+    padding: "10px",
+    gap: "10px",
+    justifyItems: "center",
+    height: "100vh",
+    alignItems: "start",
+  },
+  backButton: {
+    justifySelf: "start",
+  },
+  manualControlHeader: {
+    color: "#c0c0c0",
+    fontSize: "28px",
+  }
+};
+
+const Settings: NextPage = () => {
   return (
-    <div className={styles.container}>
-      Pest control
-      <br />
+    <div style={styles.main}>
+      <Head>
+        <title>Langhe - Pest Control</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
 
       <Link href="/">
-        <a>back</a>
+        <a style={styles.backButton}>
+          <Button variant="text" startIcon={<Icon>arrow back</Icon>}>
+            Back
+          </Button>
+        </a>
       </Link>
+      <Typography variant='h1' sx={{ fontSize: 60 }}>
+        Pest Control
+      </Typography>
+      <FormControlLabel
+        value="auto"
+        control={<Switch />}
+        label="Auto Mode"
+      />
+
+      <FormControl fullWidth>
+        <InputLabel htmlFor="component-outlined">Sulfur Level</InputLabel>
+        <Select
+          labelId="component-outlined"
+          value={"medium"}
+          label="Sulfur Level"
+          // onChange={handleChange}
+        >
+          <MenuItem value={"low"}>Low</MenuItem>
+          <MenuItem value={"Medium"}>Medium</MenuItem>
+          <MenuItem value={"Hight"}>Hight</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   )
 }
 
-export default PestControl
+export default Settings
