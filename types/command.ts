18133f2bb55439b4c4d029c12e1ee1ed6a2fec5b
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-25 06:44:33 -0400

    Added command types

---
+++ types/command.ts
@@ -0,0 +1,61 @@
+import { ControlMode } from "./greenhouse-state";
+
+export interface EnvironmentRecipeCommand {
+    day_temperature?: number;
+    night_temperature?: number;
+    humidity_limit?: number; // should be enum (hight, medium, high)?
+}
+
+export interface EnvironmentCommand {
+    mode?: ControlMode;
+    exhaust?: boolean;
+    heat?: boolean;
+    vent?: boolean;
+    recipe?: EnvironmentRecipeCommand;
+}
+
+export interface IpmRecipeCommand {
+    intensity?: number; // should be enum (hight, medium, high)?
+}
+
+export interface IpmCommand {
+    mode?: ControlMode;
+    sulfur?: boolean;
+    recipe?: IpmRecipeCommand;
+}
+
+export interface LightingRecipeCommand {
+    start_at?: string;
+    stop_at?: string;
+    intensity?: number; // should be enum (hight, medium, high)?
+}
+
+export interface LightingCommand {
+    mode?: ControlMode;
+    light?: boolean;
+    recipe?: LightingRecipeCommand;
+}
+
+export interface IrrigationRecipeZoneCommand {
+    start_window?: string;
+    stop_window?: string;
+    duration?: string;
+    frequency?: string;
+}
+
+export interface IrrigationRecipeCommand {
+    zones?: IrrigationRecipeZoneCommand[];
+}
+
+export interface IrrigationCommand {
+    mode?: ControlMode;
+    trigger_valve?: number;
+    recipe?: IrrigationRecipeCommand;
+}
+
+export interface Command {
+    environment?: EnvironmentCommand;
+    ipm?: IpmCommand;
+    lighting?: LightingCommand;
+    irrigation?: IrrigationCommand;
+}
