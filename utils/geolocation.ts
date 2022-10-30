1c28128d92c219b6faa09250a15801499cbd9521
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-10-30 16:47:57 -0400

    Home page design

---
+++ utils/geolocation.ts
@@ -0,0 +1,21 @@
+interface LatLong {
+    latitude: number;
+    longitude: number;
+}
+
+// using an interface instead of 2 arguments so that the caller should see if they matched lat with lat and long with long
+export function latitudeLongitudeDisplay(latLong: LatLong) {
+    let latLabel = "";
+    if(latLong.latitude > 0)
+        latLabel = "°N";
+    else if(latLong.latitude < 0)
+        latLabel = "°S";
+
+    let longLabel = "";
+    if(latLong.longitude > 0)
+        longLabel = "°E";
+    else if(latLong.longitude < 0)
+        longLabel = "°W";
+
+    return `${latLong.latitude.toFixed(2)}${latLabel} | ${latLong.longitude.toFixed(2)}${longLabel}`;
+}
