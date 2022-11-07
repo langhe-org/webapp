3e5fbc4f3424a4eff94d84a8257feca795511dc6
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-11-06 21:54:28 -0500

    Made app installable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import { UserContext } from '../contexts/user'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { User } from '../types/user';
 import { CacheProvider, ThemeProvider } from '@emotion/react';
 import Head from 'next/head';
@@ -14,6 +14,20 @@ export default function MyApp(props: any) {
   const [user, setUser] = useState<User>();
 
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js")
+          .then(registration => {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          })
+          .catch(err => {
+            console.log("Service Worker registration failed: ", err);
+          });
+      });
+    }
+  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
@@ -39,6 +53,7 @@ export default function MyApp(props: any) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="manifest" href="manifest.json" />
       </Head>
       <ThemeProvider theme={theme}>
         <UserContext.Provider value={{user, setUser}}>
